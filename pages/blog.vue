c738efc52e11f11451565049f4cfb07e470fe9ba
b-nimaev <moyhoroshenkiy@gmail.com>
2023-09-06 14:31:02 +0800

    update

--- pages/blog.vue
+++ pages/blog.vue
@@ -4,9 +4,14 @@
 
         <!-- <HomeBlogComponent /> -->
         <main>
+            <section class="blog-header">
+                <div class="container">
+                    <h3>Привет! Это наш блог. Сюда попадает всё самое интересное от наших экспертов.</h3>
+                </div>
+            </section>
             <div class="container">
                 <div class="post-list">
-                    <article v-for="i in 15" :key="i">
+                    <article v-for="i in 12" :key="i">
                         <BlogSlideComponent />
                     </article>
                 </div>
@@ -17,8 +22,20 @@
 </template>
 
 <style lang="scss" scoped>
+
+@import '@/assets/css/main.scss';
+
 article {
     width: 350px;
+}
+
+section {
+    &.blog-header {
+        padding: 100px 0 70px;
+        h3 {
+            font-size: 24px;
+        }
+    }
 }
 
 .post-list {
@@ -26,5 +43,6 @@ article {
     display: flex;
     gap: 30px;
     flex-wrap: wrap;
+    padding: 60px 0;
 }
 </style>
